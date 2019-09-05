e24b8eafae226d30abf5754430a9a0a8fb159fd8
Samuel Folledo <samuelfolledo@gmail.com>
2019-09-05 16:00:57 -0700

    [add] templates and this is currently working righ tnow

--- app.py
+++ app.py
@@ -3,18 +3,40 @@ from flask import Flask, request, render_template #for query string (1hr 8mins)
 
 app = Flask(__name__)
 
-compliments = ["coolio", "smashing", "neato", "fantabulous"]
+compliments = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
+    'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
+    'wonderful', 'smashing', 'lovely', 'tenacious', 'Pythonic']
+horoscopes = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"]
 
-@app.route('/compliments') #this decorator on top of the function makes it a route
+@app.route('/compliment') #this decorator on top of the function makes it a route
 def get_compliments(): #method that gives the user's name a compliment
     name = request.args.get('name') #extract a query string
+    show_compliments = request.args.get('show_compliments') #extract a query string that has a key "show_compliments"
     compliment = choice(compliments) #choose randomly from our compliments list
+
+    # num_compliments = int(request.args.get('num_compliments'))
+    # compliments_to_show = sample(compliments, num_compliments)
     return f"Hello there, {name}! You are so {compliment}!" #f lets you put the string in a variable
 
+@app.route("/horoscopes")
+def get_horoscope():
+    name = request.args.get('name') #extract a query string
+    horoscope = choice(horoscopes) #choose randomly from our compliments list
+    
+    pass
+    num_compliments = int(request.args.get('num_compliments'))
+    show_compliments = request.args.get('show_compliments')
+    compliments_to_show = sample(compliments, num_compliments)
+    return f"Hello there, {name}! You are so {compliment}!" #f lets you put the string in a variable
+
+
+
 @app.route('/')
-def index(): #index is the default home page
-    """Show the homepage and ask the user's name."""
-    return """
+def index(): #Show the homepage and ask the user's name
+    return render_template('index.html') #instead of returning a HTML form, we change our route to call the render_template function with the file containing our function 
+    
+    """
         <form action= '/compliments'>
         What is your name?
         <input type = "text" name = "name"></input>
